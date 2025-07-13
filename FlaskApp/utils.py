37c177a04351e43901d22e5bc284fbfb9c2a943e
SamraAzizi <samarraazizi@gmail.com>
2025-07-13 18:18:28 +0500

    ai web chatbot

--- FlaskApp/utils.py
+++ FlaskApp/utils.py
@@ -15,3 +15,28 @@ from tensorflow.keras.models import load_model
 
 def clean_up_sentence(sentence):
     lemmatizer = WordNetLemmatizer()
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+
+    return sentence_words
+
+
+def bag_of_words(sentence):
+    words = pickle.load(open('model/words.pkl', 'rb'))
+
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+
+def predict_class(sentence):
+    classes = pickle.load(open('model/classes.pkl', 'rb'))
+    model = load_model('model/chatbot_model.keras')
+
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
