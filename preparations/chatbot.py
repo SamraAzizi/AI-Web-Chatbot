8ec3d61810b35442acfab4fc6473c1b8365eb7fa
SamraAzizi <samarraazizi@gmail.com>
2025-07-14 17:17:40 +0500

    ai web chatbot

--- preparations/chatbot.py
+++ preparations/chatbot.py
@@ -19,7 +19,7 @@ model = load_model('model/chatbot_model.keras')
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
-Intelligent Web Chatbot
+
     return sentence_words
 
 
@@ -31,6 +31,8 @@ def bag_of_words(sentence):
             if word == w:
                 bag[i] = 1
     return np.array(bag)
+
+
 def predict_class(sentence):
     bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
@@ -45,3 +47,22 @@ def predict_class(sentence):
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
 
     return return_list
+
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+
+    return result
+
+
+while True:
+    message = input('')
+    ints = predict_class(message)
+    res = get_response(ints, intents)
+    print(res)
