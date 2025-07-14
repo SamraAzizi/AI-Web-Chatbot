6be8da901c3f2d6c740d33f14dac0956fe838f8a
SamraAzizi <samarraazizi@gmail.com>
2025-07-14 17:16:49 +0500

    ai web chatbot

--- preparations/chatbot.py
+++ preparations/chatbot.py
@@ -10,3 +10,24 @@ from tensorflow.keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
 intents = json.load(open('intents.json'))
+
+words = pickle.load(open('model/words.pkl', 'rb'))
+classes = pickle.load(open('model/classes.pkl', 'rb'))
+model = load_model('model/chatbot_model.keras')
+
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+Intelligent Web Chatbot
+    return sentence_words
+
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
