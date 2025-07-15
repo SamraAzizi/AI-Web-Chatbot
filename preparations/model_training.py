58e191bc041810a60ce3c4e253a2f8c77e0eaa36
SamraAzizi <samarraazizi@gmail.com>
2025-07-15 22:14:35 +0500

    ai web chatbot

--- preparations/model_training.py
+++ preparations/model_training.py
@@ -15,3 +15,25 @@ from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer()
+intents = json.load(open('intents.json'))
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open('model/words.pkl', 'wb'))
+pickle.dump(classes, open('model/classes.pkl', 'wb'))
